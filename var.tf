7419f7cf099b29bb250a6c95959daaee37eaabf3
Derek Kitzelman <derek.kitzelman@gmail.com>
2020-11-06 09:15:40 -0600

    Create new file
    adding variables

---
+++ var.tf
@@ -0,0 +1,15 @@
+variable "subscriptionId" {
+  type = string
+}
+
+variable "tenantId" {
+  type = string
+}
+
+variable "clientId" {
+  type = string
+}
+
+variable "clientSecret" {
+  type = string
+}
