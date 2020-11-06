00aad2215ce41e8276759fad464293fe8a5a3e20
Derek Kitzelman <derek.kitzelman@gmail.com>
2020-11-06 09:30:09 -0600

    Update var.tf
    added prefix and region variables

--- var.tf
+++ var.tf
@@ -13,3 +13,11 @@ variable "clientId" {
 variable "clientSecret" {
   type = string
 }
+
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+}
+
+variable "region" {
+  description = "The Azure Region in which all resources in this example should be created"
+}
